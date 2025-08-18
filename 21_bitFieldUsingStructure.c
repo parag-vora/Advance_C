8aeb9f22faf4f0c4b96ebc8fe7b9ea4fa763c952
parag-vora <voraparag10@gmail.com>
2025-08-18 21:36:49 +0200

    this file demonstrate the use of structure with bit field.

---
+++ 21_bitFieldUsingStructure.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/*
+    three of "var" are flags.
+    fourth value of "var" called type, which range from 1 ... 255
+    final value of "var" called index, which range from 1 ... 100000
+    */
+typedef struct bitfield{
+    unsigned int :3;
+    unsigned int f1:1;
+    unsigned int f2:1;
+    unsigned int f3:1;
+    unsigned int type:8;
+    unsigned int index:18;
+}BITFIELD;
+
+int main(){
+   
+    BITFIELD _b1;
+    _b1.f1 = 1;
+    _b1.type = 7;
+    _b1.f3 = 1;
+
+    /*
+    if the programmer don't want to think about bitwise operator and bitfield then use structure with bitfield.
+    unsigned int with bitfield has been implementd on 20_bitfieldInteger.c file
+    */
+    
+    return 0;
+}
