478e54d3b987b481090a3a5f698e1e690184fc57
parag-vora <voraparag10@gmail.com>
2025-08-19 22:06:19 +0200

    Digital to binary function has been added

--- 24_bitwiseOperatorChallenge.c
+++ 24_bitwiseOperatorChallenge.c
@@ -1,4 +1,22 @@
 #include<stdio.h>
+#include<stdbool.h>
+
+bool array[32];
+
+void DecToBin_Fun(unsigned int num){
+    bool rem = 0;
+    for (size_t i = 0; i < sizeof(int) * 8; i++)
+    {
+         unsigned int temp = num/2;
+         array[31 - i] = num%2;
+         num = temp;                 
+    }
+    for (size_t i = 0; i < sizeof(int) * 8; i++)
+    {
+        printf("%d", array[i]);
+    }
+    printf("\n");
+}
 
 int main(){
 
@@ -12,6 +30,8 @@ int main(){
     printf("%d ^ %d: %d\n", arr[0], arr[1], arr[0] ^ arr[1]);
     printf("%d<<%d : %d\n", arr[0],2 , arr[0]<<2);
     printf("%d>>%d : %d\n", arr[0],2 , arr[0]>>2);
+    DecToBin_Fun(arr[0]);
+    DecToBin_Fun(arr[1]);
 
     return 0;
 }
