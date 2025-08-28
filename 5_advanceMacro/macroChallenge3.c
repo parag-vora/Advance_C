2c2881f4ae4e9df2973d792768919a4e67b1db49
parag-vora <voraparag10@gmail.com>
2025-08-28 22:30:49 +0200

    demonstrate the use of macro with condition

---
+++ 5_advanceMacro/macroChallenge3.c
@@ -0,0 +1,28 @@
+// demonstrate the use of macro with condition
+
+#include<stdio.h>
+
+#define UPPER_CASE(X) \
+        (X>=65 && X<=90) ? 1 : 0 \
+
+    
+#define LOWER_CASE(X) \
+        (X>=97 && X<=122) ? 1 : 0 \
+   
+        
+
+int main(){
+
+    printf("Enter a char: ");
+    char ch = getchar();
+    if (UPPER_CASE(ch))
+    {
+        printf("%c is Upper case\n", ch);
+    } else if(LOWER_CASE(ch))
+    {
+        printf("%C is lower case\n", ch);
+    }
+    
+
+    return 0;
+}
