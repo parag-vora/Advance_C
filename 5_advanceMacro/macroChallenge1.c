cc755b2043b30c328da26f8cd79bebacbc63e6d3
parag-vora <voraparag10@gmail.com>
2025-08-28 21:58:25 +0200

    demonstration of different predefined macro

---
+++ 5_advanceMacro/macroChallenge1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    printf("__LINE__: %d\n", __LINE__);
+    printf("__FILE__: %s\n", __FILE__);
+    printf("__DATE__ :%s\n", __DATE__);  // mm dd yyyy
+    printf("__TIME__: %s\n", __TIME__);  //hh:mm:ss
+    printf("__STDC__: %d\n", __STDC__);
+
+    return 0;
+}
